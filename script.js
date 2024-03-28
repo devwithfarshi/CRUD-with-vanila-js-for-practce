c69f1e59ef7ba0e013e7a1d50273fc8fe7bba5e1
MD Salman Farshi <devwithfarshi@gmail.com>
2024-03-29 01:57:23 +0600

    todo ( create read and localStorage)

--- script.js
+++ script.js
@@ -1,135 +1,67 @@
-let users = [
-  {
-    id: "a",
-    name: "sumon",
-    age: 20,
-    gender: "male",
-    email: "thesumonart@gmail.com",
-    isPatner: true,
-  },
-  {
-    id: "b",
-    name: "slaman",
-    age: 29,
-    gender: "male",
-    email: "devwithfarshi@gmail.com",
-    isPatner: true,
-  },
-  {
-    id: "c",
-    name: "Rabeya",
-    age: 19,
-    gender: "female",
-    email: "rabeya@gmail.com",
-    isPatner: true,
-  },
-  {
-    id: "d",
-    name: "Abir",
-    age: 50,
-    gender: "male",
-    email: "seobyabir@gmail.com",
-    isPatner: false,
-  },
-];
+// get nassasary elements form DOM
+const addTaskBtn = document.getElementById("add_btn");
+const taskInputElement = document.getElementById("task_input");
+const task_list_element = document.querySelector(".task_list");
 
-// Delete
+// all task stror this array (tasks)
+let tasks =
+  localStorage.getItem("tasks") === null
+    ? []
+    : JSON.parse(localStorage.getItem("tasks"));
 
-// 1. splice
-// 2. filter
+// Funstionss 👇
+// unique id generator
+const uniqueId = () => {
+  const dateString = Date.now().toString(36);
+  const randomness = Math.random().toString(36).substr(2);
+  return dateString + randomness;
+};
 
-// delete user by splice
+// createTodoInUi
+const createTodoInUi = (taskName) => {
+  //   create li for visible in DOM
+  const li = document.createElement("li");
+  li.classList.add("task");
+  li.innerHTML = `<p>${taskName}</p>
+    <section class="tast_actions">
+      <div class="edit_icon">
+        <i class="fa-regular fa-pen-to-square fa-fw"></i>
+      </div>
+      <div class="delete_icon">
+        <i class="fa-solid fa-trash-can fa-fw"></i>
+      </div>
+    </section>`;
+  task_list_element.appendChild(li);
+};
 
-// const email = prompt("Enter email which you want to delete");
+const handleLoadTasks = () => {
+  tasks.map((curTask) => {
+    createTodoInUi(curTask.taskName);
+  });
+};
 
-// const value = users.find((user) => {
-//   return user.email === email;
-// });
-// if (value !== undefined) {
-//   const desicion = confirm("Are you sure you want to delete?");
-//   if (desicion === true) {
-//     const deleteItemIndexNumber = users.findIndex((item) => {
-//       return item.email === email;
-//     });
-
-//     users.splice(deleteItemIndexNumber, 1);
-//     alert("User deleted successfully");
-//   } else {
-//     console.log("You saved your account successfully!");
-//   }
-// } else {
-//   console.log(`Email (${email}) not found`);
-// }
-
-// delete user by filter
-
-// const tempUsers = users.filter((user) => {
-//   return user.id !== "b";
-// });
-
-// users = tempUsers;
-
-// const email = prompt("Enter email which you want to delete");
-
-// const value = users.find((user) => {
-//   return user.email === email;
-// });
-// if (value) {
-//   const desicion = confirm("Are you sure you want to delete?");
-//   if (desicion) {
-//     //we can write it like (desicion ===true)
-//     const tempUsers = users.filter((user) => {
-//       return user.id !== "b";
-//     });
-
-//     users = tempUsers;
-//     alert("User deleted successfully");
-//   } else {
-//     console.log("You saved your account successfully!");
-//   }
-// } else {
-//   console.log(`Email (${email}) not found`);
-// }
-
-// Create👇
-
-// const name = prompt("Enter your name");
-// const email = prompt("Enter your email");
-// const age = +prompt("Enter your age");
-// const isMale = confirm("Are you male?");
-
-// const newUser = {
-//   id: Date.now().toString(),
-//   name,
-//   age,
-//   gender: isMale ? "male" : "female",
-//   email,
-// };
-
-// users.push(newUser);
-
-//TODO: Filter by isParter
-
-// const tepmUser = users.filter((user) => {
-//   return user.isPatner;
-// });
-// users = tepmUser;
-
-//TODO: Sort by age
-
-// const tempUser = users.filter((user) => {
-//   return user.age <= 25;
-// });
-
-// users = tempUser;
-
-// Read
-console.log("===================================");
-console.log("Id | Name | Email | Gender | Age | Partner");
-users.forEach((item) => {
-  console.log(`
-${item.id} | ${item.name} | ${item.email} | ${item.gender} | ${item.age} | ${
-    item.isPatner ? "Active" : "Disabled"
+// add To do fun handleAddTask
+const handleAddTask = () => {
+  const value = taskInputElement.value;
+  if (value != "") {
+    const newTask = {
+      id: uniqueId(),
+      taskName: value,
+      complete: false,
+    };
+    tasks.push(newTask);
+    console.log(tasks);
+    createTodoInUi(newTask.taskName);
+    //   add to localstorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    taskInputElement.value = "";
+  } else {
+    alert("Required all filed!");
   }
-`);
-});
+};
+
+// add event handleAddTask
+addTaskBtn.addEventListener("click", handleAddTask);
+
+// get tasks from localstorage
+window.addEventListener("DOMContentLoaded", handleLoadTasks);
